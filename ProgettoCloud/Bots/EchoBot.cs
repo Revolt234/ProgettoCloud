785b468e5773d9a06928936510afa647eafa6cef
LORENZO SCORZELLI <l.scorzelli1@studenti.unisa.it>
2023-12-21 12:20:36 +0100

    asasas

--- ProgettoCloud/Bots/EchoBot.cs
+++ ProgettoCloud/Bots/EchoBot.cs
@@ -8,7 +8,7 @@ using Microsoft.Bot.Schema;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-////AHAHAHHAaassasas
+////AHAHAHHAaassasassdssdsddsY
 namespace ProgettoCloud.Bots
 {
     public class EchoBot : ActivityHandler
